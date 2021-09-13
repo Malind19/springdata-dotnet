af2d1936ae7cc966093e0bc83406963e4dde9b24
Malin De Silva <malindesilva@LIVE.COM>
2021-09-14 00:21:28 +0530

    Added  pulse controller

---
+++ springdata-api/Controllers/PulseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace springdata_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PulseController : ControllerBase
+    {
+        public string Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
